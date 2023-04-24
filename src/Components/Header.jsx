7210a8cc8cafd2df751100fc3beec2b92a54f9a7
majazocom <maja.gedda@zocom.se>
2023-04-24 10:56:02 +0200

    started with scss

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import style from "./Header.module.scss";
 
 function Header() {
     return ( 
-        <header>
+        <header className={style.header}>
             <h1>HEADER</h1>
-            <Link to="/cart">
+            <Link to="/cart" className={style.link}>
                 <img src="/imgs/shopping-cart.svg" width="25px" height="25px" />
             </Link>
         </header>

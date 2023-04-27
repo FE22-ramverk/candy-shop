5856730040a229b9b82a75c9caf8e2644f4042f0
majazocom <maja.gedda@zocom.se>
2023-04-27 14:11:53 +0200

    remove logging

--- src/reducers/candyReducer.js
+++ src/reducers/candyReducer.js
@@ -8,10 +8,10 @@ const candyReducer = (state = candyStore, action) => {
             let candyToAdd = action.payload;
             let objIndex = cart.findIndex((cartItem => cartItem.id === candyToAdd.id));
             if (objIndex > -1) {
-                console.log('finns');
                 // om varan redan finns i korgen vill vi plussa på antalet
                 cart[objIndex].amount += 1;
             } else {
+                // om varan inte finns i cart skall den läggas in
                 candyToAdd.amount = 1
                 console.log(candyToAdd);
                 cart.push(candyToAdd)

cda72f3af4a6c548ef51e97f7abf32643c753530
majazocom <maja.gedda@zocom.se>
2023-04-27 15:34:19 +0200

    conditionals on cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import Button from "../Components/Button";
 import CartItem from "../Components/CartItem";
 
 function Cart() {
@@ -12,11 +13,15 @@ function Cart() {
 
     return (
         <main>
-            <p>Your cart</p>
+            <h2>Your cart</h2>
+            {cart.length === 0 && <p>this looks empty...</p>}
             {
                 cart && cart.map((cartItem, i) =>
                     <CartItem cartItem={cartItem} key={i} />)
             }
+            {cart.length > 0 && <section className='buttonContainer'>
+                <Button title="CHECKOUT" action={() => console.log('checkout')} />
+            </section>}
         </main>
     );
 }

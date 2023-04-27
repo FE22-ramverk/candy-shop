4a73021ef29636c6b13742e0b5c21136c643279b
majazocom <maja.gedda@zocom.se>
2023-04-27 15:09:56 +0200

    styling on cart

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -7,17 +7,16 @@ export default function CartItem({ cartItem }) {
     const dispatch = useDispatch();
     return (
         <article className={style.container}>
-            <section className={style.itemImage}></section>
-            <div className={style.wrapper}>
-                <section className={style.info}>
-                    <h3>{cartItem.name}</h3>
-                </section>
-                <section className={style.buttons}>
-                    <button onClick={() => dispatch(removeCandy(cartItem))}>-</button>
-                    <input placeholder={cartItem.amount} onBlur={(e) => dispatch(manuallySetAmount(cartItem, e.target.value))} />
-                    <button onClick={() => dispatch(buyCandy(cartItem))}>+</button>
-                </section>
-            </div>
+            <section className={style.thumbnail} style={{ backgroundImage: 'url(/imgs/candy-' + cartItem.id + '.png)' }}></section>
+            <section className={style.info}>
+                <h3>{cartItem.name}</h3>
+            </section>
+            <section className={style.buttons}>
+                <button onClick={() => dispatch(removeCandy(cartItem))}>-</button>
+                <input placeholder={cartItem.amount} onBlur={(e) => dispatch(manuallySetAmount(cartItem, e.target.value))} />
+                <button onClick={() => dispatch(buyCandy(cartItem))}>+</button>
+            </section>
+
         </article>
     )
 }
